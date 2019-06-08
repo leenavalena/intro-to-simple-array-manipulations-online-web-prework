97774fed29b33987c47eeed1c49a1d760ecc8400
Valena Del Valle <valenadelvalle4@gmail.com>
2019-06-08 08:46:54 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -1,4 +1,5 @@
 def using_push(next_country)
+countries_in_western_africa = ["Benin", "Carpe Verde", "Gambia", "Liberia", "Mali"]
   next_country = "Niger"
     country.push("Niger")
     updated_array = using_push(countries_in_western_africa, next_country)
